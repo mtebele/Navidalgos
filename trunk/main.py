ef2ba865b38cffeb2ebcd77053562778b9817d2f
mati.tebele@gmail.com <mati.tebele@gmail.com@1f494861-6d4f-da57-5ce9-62d812d6330a>
2013-12-09 00:41:34 +0000

    +listar_fabricas en sistema

--- trunk/main.py
+++ trunk/main.py
@@ -5,27 +5,7 @@ import csv
 import juguete
 import ciudad
 import fabrica
-import operator
-
-#PASAR A SISTEMA#
-def listar_fabricas(lista_fabricas):
-	#VER SI MODIFICAR lista_fabricas O DUPLICARLA
-	lista_fabricas.sort(key=operator.attrgetter('hora_salida', 'hora_entrada'))
-	lista = []
-	cant = 0
-	fin_ant = -1
-	for i in range(len(lista_fabricas)):
-		if lista_fabricas[i].hora_entrada >= fin_ant:
-			idf = lista_fabricas[i].idf
-			hora_ent = '{:02d}:{:02d}'.format(*divmod(lista_fabricas[i].hora_entrada, 60))
-			hora_sal = '{:02d}:{:02d}'.format(*divmod(lista_fabricas[i].hora_salida, 60))			
-			lista.append(idf+','+hora_ent+','+hora_sal)
-			fin_ant = lista_fabricas[i].hora_salida
-			cant += 1
-	
-	print('Cantidad: {}').format(cant)
-	for i in range(len(lista)):
-		print(lista[i])
+import sistema
 
 def cargar_fabricas():
 	with open('fabricas.csv', 'rb') as archivo_f:
@@ -50,7 +30,7 @@ def cargar_juguetes():
 def cargar_mapa():
 	with open('mapa.csv', 'rU') as archivo_c:
 		archivo_csv = csv.reader(archivo_c)
-		city = ciudad.Ciudad()
+		mapa = Ciudad()
 		cant_esquinas = int(archivo_csv.next()[0])
 		for i in range(cant_esquinas):
 			lista = archivo_csv.next()
@@ -59,7 +39,7 @@ def cargar_mapa():
 			y = float(lista[2])
 			latitud = float(lista[3])
 			longitud = float(lista[4])
-			city.agregar_esquina(ide, x, y, latitud, longitud)
+			mapa.agregar_esquina(ide, x, y, latitud, longitud)
 		cant_calles = int(archivo_csv.next()[0])
 		for j in range(cant_calles):
 			lista = archivo_csv.next()
@@ -68,34 +48,23 @@ def cargar_mapa():
 			esq_fin = lista[2]
 			city.agregar_calle(idc, esq_inicio, esq_fin)
 	archivo_c.close()
-	return city
+	return mapa
 
-def validar_instruccion(instruccion):	
-	"""Valido si la instruccion tiene formato correcto"""
-	validos = { "listar_fabricas" : 0,
-                "valuar_juguetes" : 1,
-                "valuar_juguetes_total" : 0,
-                "camino_optimo" : 1,
-                "listar_juguetes" : 1,
-                "entrenar_duendes" : 1,
-                "graficar_rutas" : 1,
-	}
-	if instruccion[0] in validos.keys():
-		return validos[instruccion[0]] == len(instruccion[1])
-	else: 	
-		return False
-
-def main():
+def main():	
 	polo = int(sys.argv[1]);
 	capacidad = int(sys.argv[2]);
 	lista_fabricas = []
+	mapa = ""
+	lista_juguetes = []
 	for i in range(len(sys.argv)-3):
 		if sys.argv[i+3] == '/home/tebele/Desktop/TP3/pruebas/fabricas.csv': #corregir esta mierda
 			lista_fabricas = cargar_fabricas()
 		elif sys.argv[i+3] == 'juguetes.csv':
-			cargar_juguetes()
+			lista_juguetes = cargar_juguetes()
 		elif sys.argv[i+3] == 'mapa.csv':
-			cargar_mapa()
+			mapa = cargar_mapa()
+			
+	sis = sistema.Sistema(polo, capacidad, ciudad, lista_juguetes, lista_fabricas)
 			
 	#print('CARGA FINALIZADA')
 	
@@ -112,16 +81,10 @@ def main():
 			resto_linea = linea[pos_espacio+1:]
 			parametros = resto_linea.split(",")
 		
-		instruccion = (comando, parametros)	
-			
-		#if validar_instruccion(instruccion):
-		#	print "Comando valido"
-		#else: print "Comando Invalido" 
-		#print(instruccion[0])
+		instruccion = (comando, parametros)
 		
 		#COMIENZO INSTRUCCIONES#
 		if instruccion[0] == 'listar_fabricas':
-			listar_fabricas(lista_fabricas)
-		
+			sis.listar_fabricas()		
 		
 main()
--- trunk/sistema.py
+++ trunk/sistema.py
@@ -1,3 +1,5 @@
+import operator
+
 class Sistema:
     def __init__(self, pn, capacidad, mapa, juguetes, fabricas):
 		self.polo_norte = pn
@@ -6,15 +8,31 @@ class Sistema:
 		self.juguetes = juguetes
 		self.fabricas = fabricas
 	
-	def listar_fabricas(self)
+    def listar_fabricas(self):
+		lista_fabricas = sorted(self.fabricas, key=operator.attrgetter('hora_salida', 'hora_entrada'))
+		lista = []
+		cant = 0
+		fin_ant = -1
+		for i in range(len(lista_fabricas)):
+			if lista_fabricas[i].hora_entrada >= fin_ant:
+				idf = lista_fabricas[i].idf
+				hora_ent = '{:02d}:{:02d}'.format(*divmod(lista_fabricas[i].hora_entrada, 60))
+				hora_sal = '{:02d}:{:02d}'.format(*divmod(lista_fabricas[i].hora_salida, 60))			
+				lista.append(idf+','+hora_ent+','+hora_sal)
+				fin_ant = lista_fabricas[i].hora_salida
+				cant += 1
+		
+		print('Cantidad: {}').format(cant)
+		for i in range(len(lista)):
+			print(lista[i])
 
-	def valuar_carrito(self)
+    def valuar_carrito(self): return
 
-	def valuar_juguetes_total(self)
-	
-	def camino_optimo(self, idf)
+    def valuar_juguetes_total(self): return
 
-	def listar_juguetes(self, idf)
+    def camino_optimo(self, idf): return
 
-	def graficar_rutas(self, idf)
+    def listar_juguetes(self, idf): return
+
+    def graficar_rutas(self, idf): return
 
