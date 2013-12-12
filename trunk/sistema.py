af1afce6d3bdc398a52e5538e9d81ee848d681f8
lucas.lerp@gmail.com <lucas.lerp@gmail.com@1f494861-6d4f-da57-5ce9-62d812d6330a>
2013-12-12 21:58:55 +0000

    agregado graficar_rutas. Si podés darle un poco de formato y modularizacion, mejor.

--- trunk/sistema.py
+++ trunk/sistema.py
@@ -1,6 +1,7 @@
 import operator
 from grafo import Grafo
 from ciudad import Ciudad
+from fabrica import Fabrica
 
 class Sistema:
     def __init__(self, pn, capacidad, ciudad, fabricas):
@@ -10,7 +11,10 @@ class Sistema:
 		self.fabricas = fabricas
 		self.fabricas_visitar = None
 	
-    def listar_fabricas(self):																											
+    def listar_fabricas(self):
+		"""
+		Devuelve las fabricas optimas en cuanto a cantidad de fabricas visitadas. 
+		"""																	
 		lista_fabricas = sorted(self.fabricas, key=operator.attrgetter('hora_salida', 'hora_entrada'))
 		lista = []
 		lista_visitar = []
@@ -30,6 +34,10 @@ class Sistema:
 		return lista
 		
     def valuar_juguetes(self, idf, listar_juguetes = False):
+		"""
+		Devuelve el valor de sonrisas correspondiente a la combinacion de 
+		juguetes optima a retirar de la fabrica <idf>
+		"""
 		if idf >= len(self.fabricas):
 			print('Error: la fabrica con id ' + idf + ' no existe')
 			return
@@ -59,6 +67,10 @@ class Sistema:
 		return valor
     
     def valuar_juguetes_total(self):
+		"""
+		Devuelve los valores de sonrisas optimos para todas las fabricas 
+		a visitar
+		"""
 		if self.fabricas_visitar is None:
 			self.listar_fabricas()
 		total_sonrisas = 0
@@ -67,15 +79,23 @@ class Sistema:
 		return total_sonrisas
 
     def camino_optimo(self, idf):
+		"""
+		Devuelve la distancia optima desde la fabrica <idf> hasta el polo norte
+		y las coordenadas angulares del recorrido efectuado.
+		"""
 		fin = self.polo_norte
-		camino, distancia = self.ciudad.camino_optimo(idf, fin)
-		return camino, distancia #aca obtenemos los nombres de las ciudades nada mas, habria que darle formato.
-		
+		fabrica = self.fabricas[int(idf)]
+		inicio = fabrica.obtener_esquina()
+		camino, distancia = self.ciudad.camino_optimo(inicio, fin)
+		coordenadas = []
+		for c in camino:
+			coordenadas.append(self.ciudad.coordenadas_angulares(c))
+		return coordenadas, distancia
+
     def listar_juguetes(self, idf):
 		valor,res = self.valuar_juguetes(idf, True)
 		fabrica = self.fabricas[idf]
 		juguetes = fabrica.juguetes
-		
 		lista = []
 		w = self.capacidad_carrito
 		for j in range(len(juguetes)-1, 0, -1):
@@ -84,9 +104,56 @@ class Sistema:
 				jug = juguetes[j]
 				lista.append(jug)
 				w -= jug.peso
-
 		lista.reverse()
 		return valor,lista
 
-    def graficar_rutas(self, idf): return
-    
+    def graficar_rutas(self, idf):
+		coordenadas, distancia = self.camino_optimo(idf)
+		f = open(idf+'.kml', 'w')
+		f.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+		f.write("<kml xmlns=\"http://www.opengis.net/kml/2.2\"\n")
+		f.write("xmlns:gx=\"http://www.google.com/kml/ext/2.2\"\n")
+		f.write("xmlns:kml=\"http://www.opengis.net/kml/2.2\"\n")
+		f.write("xmlns:atom=\"http://www.w3.org/2005/Atom\">\n")
+		f.write("<Document>\n")
+		linea = ("	<name> {} </name>\n").format(idf)
+		f.write(linea)
+		f.write("		<Placemark>\n")
+		f.write("			<name> Viaje a fabrica </name>\n")
+		linea = ("			<description> Recorrido de la fabrica {} </description>\n").format(idf)
+		f.write(linea)
+		f.write("			<LineString>\n")
+		f.write("				<coordinates>\n")
+		for c in coordenadas:
+			linea = ("					{},{}\n").format(c[0], c[1])
+			f.write(linea)
+		f.write("			</coordinates>\n")
+		f.write("		</LineString>\n")
+		f.write("	</Placemark>\n")		
+		
+		f.write("	<Placemark>\n")
+		f.write("		<name>Polo Norte</name>\n")
+		f.write("		<description> Papa Noel </description>\n")
+		f.write("		<Point>\n")
+		polo = self.ciudad.coordenadas_angulares(str(self.polo_norte))
+		linea = ("			<coordinates>{},{}</coordinates>\n").format(polo[0],polo[1])
+		f.write(linea)
+		f.write("		</Point>\n")
+		f.write("	</Placemark>\n")
+
+		f.write("	<Placemark>\n")
+		linea = ("		<name>{}</name>\n").format(idf)
+		f.write(linea)
+		f.write("		<description> Juguetes! </description>\n")
+		f.write("		<Point>\n")
+		fabrica = self.fabricas[int(idf)]
+		esquina = fabrica.obtener_esquina()		
+		polo = self.ciudad.coordenadas_angulares(str(esquina))
+		linea = ("			<coordinates>{},{}</coordinates>\n").format(polo[0],polo[1])
+		f.write(linea)
+		f.write("		</Point>\n")
+		f.write("	</Placemark>\n")
+		f.write("</Document>\n")
+		f.write("</kml>\n")	
+		f.close()
+
