3ae922be478d11d13fffd564475514d997da618c
lucas.lerp@gmail.com <lucas.lerp@gmail.com@1f494861-6d4f-da57-5ce9-62d812d6330a>
2013-12-12 21:29:22 +0000

    Documentaciones agregadas y camino optimo bien implementado

--- ciudad.py
+++ ciudad.py
@@ -10,6 +10,9 @@ class Ciudad:
 		self.coord_metros = {}
 
 	def obtener_mapa(self):
+		"""
+		Devuelve el mapa de la ciudad
+		"""
 		return self.mapa
 
 	def calcular_distancia(self, esquina1, esquina2):
@@ -26,7 +29,7 @@ class Ciudad:
 		"""
 		Agrega la esquina <esquina> con las coordenadas
 		(x,y), (lat, lon)
-		"""		
+		"""			
 		self.mapa.agregar_vertice(esquina)
 		self.coord_metros[esquina] = np.array([x,y])
 		self.coord_angulares[esquina] = np.array([lat,lon])
@@ -37,7 +40,24 @@ class Ciudad:
 		"""
 		dist = self.calcular_distancia(esquina1, esquina2)
 		self.mapa.agregar_arista(esquina1, esquina2, dist, idc)
+	
+	def coordenadas_angulares(self, esquina):
+		"""
+		Devuelve las coordenadas (latitud y longitud) de una esquina de la ciudad
+		"""
+		return self.coord_angulares[esquina]
 
 	def camino_optimo(self, esquina_inicio, esquina_fin):
+		"""
+		Devuelve un listado con las esquinas correspondientes al camino optimo
+		desde <esquina_inicio> hasta <esquina_fin>, y la distancia recorrida
+		"""
 		camino, distancia = self.mapa.camino_minimo(esquina_inicio, esquina_fin)
 		return camino, distancia
+	
+	def esquina_pertenece(self, esquina):
+		"""
+		Determina si una esquina pertenece a la ciudad
+		"""
+		return self.mapa.pertenece(esquina)
+
